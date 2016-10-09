cc7869f97a6fd30d6f35cd2d6b3eb7299ff7441e
Ted Zhu <tedz2usa@gmail.com>
2016-10-09 15:16:48 -0400

    Print axis.

--- main.js
+++ main.js
@@ -15,7 +15,16 @@ function init() {
   ctx.fillRect(tx(20), ty(20), 4, 4);
 
   print_line(10, 10, 50, 50);
+  print_x_axis();
+  print_y_axis();
+}
 
+function print_x_axis() {
+  print_line(tx(-20), ty(0), tx(20), ty(0));
+}
+
+function print_y_axis() {
+  print_line(tx(0), ty(-20), tx(0), ty(20));
 }
 
 function print_line(x1, y1, x2, y2) {
