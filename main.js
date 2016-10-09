cee0eb34a9879318eaadb991bbba411cb81babb6
Ted Zhu <tedz2usa@gmail.com>
2016-10-09 15:49:27 -0400

    Print grid axis tick marks.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ window.onload = init;
 var canvas, ctx;
 var xMin, xMax, yMin, yMax;
 var xScale, yScale;
+var xGrid, yGrid;
 
 function init() {
 
@@ -13,8 +14,10 @@ function init() {
 
   xMin = -40;
   xMax = 200;
+  xGrid = 5;
   yMin = -80;
   yMax = 80;
+  yGrid = 5;
 
   xScale = canvas.width / (xMax - xMin);
   yScale = canvas.height / (yMax - yMin);
@@ -31,10 +34,28 @@ function init() {
 
 function print_x_axis() {
   print_line(tx(xMin), ty(0), tx(xMax), ty(0));
+
+  for (var x = 0; x > xMin; x-=xGrid) {
+    print_line(tx(x), ty(0)-4, tx(x), ty(0)+4);
+  }
+
+  for (var x = 0; x < xMax; x+=xGrid) {
+    print_line(tx(x), ty(0)-4, tx(x), ty(0)+4);
+  }
+
 }
 
 function print_y_axis() {
   print_line(tx(0), ty(yMin), tx(0), ty(yMax));
+
+  for (var y = 0; y > yMin; y-=yGrid) {
+    print_line(tx(0)-4, ty(y), tx(0)+4, ty(y));
+  }
+
+  for (var y = 0; y < yMax; y+=yGrid) {
+    print_line(tx(0)-4, ty(y), tx(0)+4, ty(y));
+  }
+
 }
 
 function print_line(x1, y1, x2, y2) {
