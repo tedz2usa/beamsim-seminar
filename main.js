c70e17b07213bbb5b2f37038df4556c4af3e4d31
Ted Zhu <tedz2usa@gmail.com>
2016-10-09 15:07:07 -0400

    Create print_line function.

--- main.js
+++ main.js
@@ -14,6 +14,15 @@ function init() {
   ctx.fillStyle = "orange";
   ctx.fillRect(tx(20), ty(20), 4, 4);
 
+  print_line(10, 10, 50, 50);
+
+}
+
+function print_line(x1, y1, x2, y2) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.stroke();
 }
 
 function tx(x) {
