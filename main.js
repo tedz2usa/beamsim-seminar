d283aeb7e8d091d1a4e701a5a53a940703efbcfc
Ted Zhu <tedz2usa@gmail.com>
2016-10-09 14:59:02 -0400

    Add coordinate transformation functions.

--- main.js
+++ main.js
@@ -2,11 +2,26 @@ var log = console.log.bind(console);
 
 window.onload = init;
 
+var canvas, ctx;
+
 function init() {
 
-  print_squares(1, 8);
-  print_squares(1, 18);
+  canvas = document.getElementById("myCanvas");
+  ctx = canvas.getContext("2d");
+  log(canvas);
+  log(ctx);
 
+  ctx.fillStyle = "orange";
+  ctx.fillRect(tx(20), ty(20), 4, 4);
+
+}
+
+function tx(x) {
+  return x;
+}
+
+function ty(y) {
+  return canvas.height - y;
 }
 
 function print_squares(start, end) {
