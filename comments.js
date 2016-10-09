779c18a6ea2d897351281a9e0688a8195e1dcd13
Ted Zhu <tedz2usa@gmail.com>
2016-10-09 14:25:40 -0400

    Add working stylesheet.

---
+++ comments.js
@@ -0,0 +1,40 @@
+// var CA = document.getElementById("content_a");
+// var CB = document.getElementById("content_b");
+// var CC = document.getElementById("content_c");
+// log(CA);
+// log(CB);
+// log(CC);
+//
+// CA.innerHTML = CA.innerHTML + " Added text.";
+// CB.innerHTML = "Replaced Text";
+// CC.style.color = "rgb(255,128,0)";
+// CC.style.fontFamily = "Consolas";
+// CC.style.fontSize = "72px";
+// CC.style.fontWeight = "bold";
+// CC.style.fontStyle = "italic";
+// CC.style.textDecoration = "underline";
+
+// var age = 2;
+//
+// if (age < 15) {
+//   log("You can't drive!");
+//   log("You can't  drive!");
+//   log("You can't   drive!");
+// } else {
+//   log("You can drive!");
+//   log("You can   drive!");
+//   log("You can      drive!");
+// }
+
+
+// var hello = 5;
+// hello++;
+// hello++;
+// hello++;
+// hello++;
+// log(hello);
+
+Hello from HTML!
+<div id="content_a">This is content A.</div>
+<div id="content_b">This is content B.</div>
+<div id="content_c">This is content C.</div>
--- main.js
+++ main.js
@@ -3,20 +3,14 @@ var log = console.log.bind(console);
 window.onload = init;
 
 function init() {
-  var CA = document.getElementById("content_a");
-  var CB = document.getElementById("content_b");
-  var CC = document.getElementById("content_c");
-  log(CA);
-  log(CB);
-  log(CC);
 
-  CA.innerHTML = CA.innerHTML + " Added text.";
-  CB.innerHTML = "Replaced Text";
-  CC.style.color = "rgb(255,128,0)";
-  CC.style.fontFamily = "Consolas";
-  CC.style.fontSize = "72px";
-  CC.style.fontWeight = "bold";
-  CC.style.fontStyle = "italic";
-  CC.style.textDecoration = "underline";
+  print_squares(1, 8);
+  print_squares(1, 18);
 
+}
+
+function print_squares(start, end) {
+  for (var i = start; i <= end; i++) {
+    log(i*i);
+  }
 }
