b1bf9de7e775df7549cb3bbbb783691484303cab
Ted Zhu <tedz2usa@gmail.com>
2016-10-09 12:15:56 -0400

    Modifying HTML elements with JavaScript.

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log("Hello World, from JavaScript!");
+var log = console.log.bind(console);
+
+window.onload = init;
+
+function init() {
+  var CA = document.getElementById("content_a");
+  var CB = document.getElementById("content_b");
+  var CC = document.getElementById("content_c");
+  log(CA);
+  log(CB);
+  log(CC);
+
+  CA.innerHTML = CA.innerHTML + " Added text.";
+  CB.innerHTML = "Replaced Text";
+  CC.style.color = "rgb(255,128,0)";
+  CC.style.fontFamily = "Consolas";
+  CC.style.fontSize = "72px";
+  CC.style.fontWeight = "bold";
+  CC.style.fontStyle = "italic";
+  CC.style.textDecoration = "underline";
+
+}
